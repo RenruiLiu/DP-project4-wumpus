06866d2fe2190d3407c4e213f25c4e206d29417b
Ryan 瑞兽瑞兽 <liur@deakin.edu.au>
2018-05-11 20:52:55 +1000

    1 InitialState
    Set Borders of map

--- nonsense.pl
+++ nonsense.pl
@@ -8,7 +8,7 @@ action(south).
 action(east).
 action(north).
 
-q([A|R]):-
+q([A|_]):-
     (A == west ->
         write("OK");
         write("NO")
@@ -19,3 +19,50 @@ q([A|R]):-
         ).
  k((A,B),X):-
     X = (A+1,B+1).
+
+
+
+
+ini(NR,NC, XS, YS, State0):- 
+    NNR is NR + 1,
+    NNC is NC + 1,
+    bor(NNR,NNC,List),
+    append(List,[(XS,YS)],State),
+    sort(State,State0).
+
+bor(NR,NC,List):-
+    borders(NR,NC,[],List1),
+    borders1(NR,NC,[],List2),
+    borders(NR,0,[],List3),
+    borders1(0,NC,[],List4),
+    append(List1,List2,ListX),
+    append(List3,List4,ListY),
+    append(ListX,ListY,List).
+
+borders(NR,NC,A,List):- 
+(   NR =:= -1 ->
+        List = A;
+    NR > -1,
+        append([(NR,NC)], A, A1),
+        NewNR is NR - 1,
+        borders(NewNR,NC,A1,List)
+).
+borders1(NR,NC,A,List):- 
+(   NC =:= -1 ->
+        List = A;
+    NC > -1,
+        append([(NR,NC)], A, A1),
+        NewNC is NC - 1,
+        borders1(NR,NewNC,A1,List)
+).
+
+
+
+
+cd(N):-
+    (N>0 ->
+    write(N),
+    NewN is N-1,
+    cd(NewN);
+    write("finished")
+    ).
--- wumpus.pl
+++ wumpus.pl
@@ -8,8 +8,13 @@
 
 :- module(wumpus,[initialState/5, guess/3, updateState/4]).
 
+% Done
 initialState(NR, NC, XS, YS, State0):-
-    State0 = [(NR,0),(0,NC),(NR,NC),(XS,YS)],%要改，从（0，0）到（NR,0）到（0,NC）再到（NR,NC）都进state里
+    BorderR is NR + 1,
+    BorderC is NC + 1,
+    bor(BorderR,BorderC,List), % The borders should be larger than the map size by 1.
+    append(List,[(XS,YS)],State),
+    sort(State,State0), % Remove depulicates
     write(State0).
 
 guess(State0, State, Guess):- 
@@ -45,3 +50,31 @@ move( (X,Y) , Action , NewPosition):-
                 NewY is Y-1,
                 NewPosition = (X,NewY)
             ).
+
+% Functions for setting borders of the map
+
+bor(NR,NC,List):-
+    borders(NR,NC,[],List1),
+    borders1(NR,NC,[],List2),
+    borders(NR,0,[],List3),
+    borders1(0,NC,[],List4),
+    append(List1,List2,ListX),
+    append(List3,List4,ListY),
+    append(ListX,ListY,List).
+
+borders(NR,NC,A,List):- 
+(   NR =:= -1 ->
+        List = A;
+    NR > -1,
+        append([(NR,NC)], A, A1),
+        NewNR is NR - 1,
+        borders(NewNR,NC,A1,List)
+).
+borders1(NR,NC,A,List):- 
+(   NC =:= -1 ->
+        List = A;
+    NC > -1,
+        append([(NR,NC)], A, A1),
+        NewNC is NC - 1,
+        borders1(NR,NewNC,A1,List)
+).
