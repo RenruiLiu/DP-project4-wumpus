029faece545d4700fcb4c873b1b595714aea7b97
Ryan 瑞兽瑞兽 <liur@deakin.edu.au>
2018-05-12 21:39:19 +1000

    2 robot travel the map

--- wumpus.pl
+++ wumpus.pl
@@ -1,10 +1,15 @@
 % Declarative Programming Project 4
-
+%
 % This program is for playing wumpus game 
 % which is basically sending robots to hunt wumpus
-
+%
 % By Renrui Liu, SID 950392, renruil@student.unimelb.edu.au
-
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% TODO:
+% 1.好像目前来讲，border不需要，因为只是在map的coordinates里走，不碰到边界，但暂时不删
+% 2.机器人从A到B只有一条线路走
+% 3.写updateState让机器人能重复开拓地图
+% 4.找到wumpus后调整开枪
 
 :- module(wumpus,[initialState/5, guess/3, updateState/4]).
 
@@ -13,31 +18,87 @@ initialState(NR, NC, XS, YS, State0):-
     BorderR is NR + 1,
     BorderC is NC + 1,
     bor(BorderR,BorderC,List), % The borders should be larger than the map size by 1.
-    append(List,[(XS,YS)],State),
-    sort(State,State0), % Remove depulicates
-    write(State0).
+    sort(List,List0),% Remove depulicates
+    append(List0,[(XS,YS)],State0).
 
 guess(State0, State, Guess):- 
-    action(A),
-    Guess = [A],
-    move(State0,A,NewState),
-    append([NewState],State0,State1),
-%    \+ member(NewState,State),
-%    append([NewState],State,State1),
-    State is State1.
-%    guess(NewState,State1,Guess).
+    %Get all coordinates of the map and pick up all possible destinations
+    allcoordinates(State0,Allcoordinates),
+    exploreMap(Allcoordinates,State0,Destinations),
 
-%    Guess = [east,east,east,south,shoot].
+    member(Destination,Destinations), % Pick one possible destination
+    last(State0,StartPoint), % Get the StartPoint
+    find(StartPoint,Destination,Path,Previous),
+    append(State0,Previous,State1), 
+    sort(State1,State), % Remove depulicates and get new state
+    append(Path,[shoot],Guess). %
 
-updateState(State0, Guess, Feedback, State).
+% Call after a robot got killed or finishied. %Not start yet
+updateState(State0, Guess, Feedback, State):-
+    write(Feedback).
 
 
-action(west).
-action(south).
-action(east).
-action(north).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-move( (X,Y) , Action , NewPosition):-
+%remove all visited coordinates from allcoordinates list.
+exploreMap(AllCords,State0,Destinations):-
+    (   State0 == [] ->
+            AllCords = Destinations;
+            State0 = [Head|Tail],
+            delete(AllCords,Head,D1),
+            exploreMap(D1,Tail,Destinations)
+    ).
+
+allcoordinates(State0,AllCords):-
+    reverse(State0,State),
+    State = [_|Rest],
+    Rest = [A|_],
+    A = (X,Y), % Get the NR and NC
+    getCords(X,Y,AllCords,[]).
+
+getCords(X,Y,AllCords,A):-
+    (   X =:= 0 ->
+            AllCords = A;
+        X > 0 ->
+            yLoop(X,Y,A1,[]),
+            NX is X - 1,
+            append(A,A1,A2),
+            getCords(NX,Y,AllCords,A2)
+        ).
+yLoop(X,Y,AllCords,A):-
+    (   Y =:= 0 ->
+            AllCords = A;
+        Y > 0 ->
+            append([(X,Y)],A,A1),
+            NY is Y - 1,
+            yLoop(X,NY,AllCords,A1)
+        ).
+
+
+%方法2，算End和Start差距，然后走
+%算出在给定范围内走到End的所有方式，并且不重复
+%Functions for calculating how many path that
+% can move a robot from coordinate A to coordinate B
+
+find(Start, Start, [],[]).
+find((X,Y), (XS,YS), Path, Previous):-
+    XD is XS - X,
+    YD is YS - Y,
+    xPath(XD,P1,[]),
+    yPath(YD,P2,[]),
+    append(P1,P2,Path), %A到B只有一条Path，需要多Paths?
+    move((X,Y),Path,Previous,[]).
+
+move( Start, Path, Previous,A):-
+    (   Path == [] ->
+            Previous = A;
+        Path = [Action|Rest],
+        move1(Start,Action,NewPosition),
+        append([NewPosition],A,A1),
+        move(NewPosition,Rest,Previous,A1)
+        ).
+
+move1( (X,Y) , Action , NewPosition):-
   (   Action == west ->
         NewX is X - 1,
         NewPosition = (NewX,Y);
@@ -45,14 +106,49 @@ move( (X,Y) , Action , NewPosition):-
             NewX is X+1,
             NewPosition = (NewX,Y);
             Action == north ->
-                NewY is Y+1,
-                NewPosition = (X,NewY);
                 NewY is Y-1,
+                NewPosition = (X,NewY);
+                NewY is Y+1,
                 NewPosition = (X,NewY)
-            ).
+).
+
+xPath(XD,Path,A):-    
+    (   XD =:= 0 ->
+            Path = A;
+            XD > 0 ->
+            append([east],A,A1),
+            NXD is XD - 1,
+            xPath(NXD,Path,A1);
+                append([west],A,A1),
+                NXD is XD + 1,
+                xPath(NXD,Path,A1)
+    ).
+yPath(YD,Path,A):-    
+    (   YD =:= 0 ->
+            Path = A;
+            YD > 0 ->
+            append([south],A,A1),
+            NYD is YD - 1,
+            yPath(NYD,Path,A1);
+                append([north],A,A1),
+                NYD is YD + 1,
+                yPath(NYD,Path,A1)
+    ).
+
+
+/* 
+action((X,Y),west,(NX,Y)):-
+    NX is X - 1.
+action((X,Y),south,(X,NY)):-
+    NY is Y + 1.
+action((X,Y),east,(NX,Y)):-
+    NX is X + 1.
+action((X,Y),north,(X,NY)):-
+    NY is Y - 1.
+*/
+
 
 % Functions for setting borders of the map
-
 bor(NR,NC,List):-
     borders(NR,NC,[],List1),
     borders1(NR,NC,[],List2),
